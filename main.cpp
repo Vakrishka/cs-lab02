4bd0cf6666af14f7e33fc6f75da2d34017c333a4
Bob (KrivorotovVV) <KrivorotovVV@mpei.ru>
2022-09-19 00:50:19 +0300

    Добавлен вывод печати произведения в машине Боба

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Вадим on 07.09.2022.
+//
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter A and B: ";
+    int a,b;
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n';
+    
+}
