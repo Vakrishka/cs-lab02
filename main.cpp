3662d32ac1079e223b910ebe974fc0947f012df4
Bob (KrivorotovVV) <KrivorotovVV@mpei.ru>
2022-09-19 03:23:20 +0300

    Минимум

--- main.cpp
+++ main.cpp
@@ -13,9 +13,18 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-    int a,b;
+    int a, b;
     cin >> a >> b;
+    if (a<b)
+            cout <<"Min = " << a << '\n';
+        else
+            cout <<"Min = " << b << '\n';
+
+    cout << "Sum: " << a + b << endl;
+    cout << "Raz: " << a - b << endl;
+    cout << "Proiz: " <<a*b<< endl;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
-         << "A * B = " << a * b << '\n';
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n';
 }
